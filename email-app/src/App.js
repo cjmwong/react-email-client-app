87a2891ee8b8e67d72db1d8a9fb30429a8d86948
wongcj247 <wongc247@mymacewan.ca>
2024-01-29 00:05:37 -0700

    colours of email tiles now change based on read status

--- email-app/src/App.js
+++ email-app/src/App.js
@@ -6,7 +6,7 @@ import Content from "./components/content/content.component";
 function App() {
   const [emails, setEmails] = useState([]);
   const [content, setContent] = useState([]);
-  console.log("hi");
+
   useEffect (() => {
     const fetchEmails = async () => {
       const response = await fetch("https://gist.githubusercontent.com/mrchenliang/15e1989583fd6e6e04e1c49287934c91/raw/ed03cfea1e2edb0303543d2908cd7429ed75580d/email.json");
@@ -17,7 +17,19 @@ function App() {
   }, []);
 
   const handleSidebarClick = (emailID) => {
-    setContent(emails.find(email => email.id === emailID));
+    const email = emails.find(email => email.id === emailID);
+    setContent(email);
+    const updatedEmails = emails.map(email => {
+      if (email.id === emailID) {
+        email.read = "active";
+      } else if (email.read === "active") {
+        email.read = "true"
+      }
+
+      return email;
+    });
+    console.log(updatedEmails);
+    setEmails(updatedEmails);
   }
 
   return (
@@ -38,9 +50,6 @@ function App() {
           <EmailList emails = {emails} onSidebarClick={handleSidebarClick}/>
         </div>
         <div class='Main'>
-          <p>
-            Body View
-          </p>
           <Content email = {content}/>
         </div>
       </div>
--- email-app/src/components/sidebar/emailTile/emailTile.component.jsx
+++ email-app/src/components/sidebar/emailTile/emailTile.component.jsx
@@ -3,13 +3,25 @@ import './emailTile.styles.css';
 
 export const Tile = ({ email, onClick }) => {
 
-    const {id, from, address, time, subject } = email;
+    const {id, from, address, time, subject, read } = email;
 
     const handleClick = () => {
         onClick(id);
+
     };
+
+    const tile = ((read) => {
+        if (read === "false") {
+            return 'email-tile';
+        } else if (read === "active") {
+            return 'email-tile active';
+        } else {
+            return 'email-tile read';
+        }
+    });
+
     return (
-        <div className='email-tile' onClick={handleClick}>
+        <div className={tile(read)} onClick={handleClick}>
             <h2 id='header'>{from}</h2>
             <p>{subject}</p>
             <p>{address}</p>
